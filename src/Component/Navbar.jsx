ace2b5f834cd1963837c689f385d7d1aa8e8f39d
unknown <sumaiya.rrashid@gmail.comm>
2023-04-10 15:05:40 +0200

    navabr added

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,54 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className='container mx-auto lg:px-28 my-2 lg:my-5'>
-            
+
             <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded">
-        <li><a>Item 1</a></li>
-        <li tabIndex={0}>
-          <a className="justify-between">
-            Parent
-            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-          </a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <Link to='/' className=" normal-case text-xl font-bold">Career <span className='text-green-500'>Connect</span></Link>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <a>
-          Parent
-          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul className="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-        </ul>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Get started</a>
-  </div>
-</div>
-            
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 gap-7 p-2 shadow bg-base-100  rounded">
+                            <Link to='statistics'>Statistics</Link>
+                            <Link to='appliedjobs'>Applied Jobs</Link>
+                            <Link to='blogs'>Blogs</Link>
+                        </ul>
+                    </div>
+                    <Link to='/' className=" normal-case text-lg lg:text-xl font-bold">Career <span className='text-green-500'>Connect</span></Link>
+                </div>
+                <nav className="navbar-center hidden lg:flex ">
+                    <ul className="menu menu-horizontal px-10 gap-10">
+                        <NavLink className={({isActive})=> (isActive? 'text-green-500': '')} to='statistics'>Statistics</NavLink>
+                        <NavLink className={({isActive})=> (isActive? 'text-green-500': '')} to='appliedjobs'>Applied Jobs</NavLink>
+                        <NavLink className={({isActive})=> (isActive? 'text-green-500': '')} to='blogs'>Blogs</NavLink>
+                    </ul>
+                </nav>
+                <div className="navbar-end">
+                    <a className="btn">Start Applying</a>
+                </div>
+            </div>
+
         </div>
     );
 };
