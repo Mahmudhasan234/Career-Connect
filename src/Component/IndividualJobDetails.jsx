19da9adac2b78ed87e72b3ed3d9ad5c26d45d3e0
unknown <sumaiya.rrashid@gmail.comm>
2023-04-11 16:42:54 +0200

    individual page done

--- src/Component/IndividualJobDetails.jsx
+++ src/Component/IndividualJobDetails.jsx
@@ -2,36 +2,90 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import Lottie from "lottie-react";
 import animation from '../assets/138485-3d-website-launch.json'
-
+import { CurrencyDollarIcon, CalendarDaysIcon, PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/solid'
 import { useLoaderData, useParams } from 'react-router-dom';
 
-const IndividualJobDetails = ({params}) => {
+const IndividualJobDetails = ({ params }) => {
     const jobs = useLoaderData()
-   const jobId = useParams(params)
-   const JobIdString =JSON.stringify(jobId)
-   const JobIdNumber =parseInt(JobIdString[7])
+    const jobId = useParams(params)
+    const JobIdString = JSON.stringify(jobId)
+    const JobIdNumber = parseInt(JobIdString[7])
 
- const [job, setJob]= useState({});
- useEffect(()=>{
-    const individualJobDetails = jobs.find(job => job.id === JobIdNumber)
-    setJob(individualJobDetails)
-    
- },[])
-  
-   console.log(JobIdNumber)
-   console.log(jobs)
-    
+    const [job, setJob] = useState({});
+    useEffect(() => {
+        const individualJobDetails = jobs.find(job => job.id === JobIdNumber)
+        setJob(individualJobDetails)
+
+    }, [])
+
+    console.log(JobIdNumber)
+    console.log(jobs)
+
     return (
         <div>
-            <div >
-                <div>
-               ;
+            <div className='lg:px-28 my-2 lg:my-5 lg:mb-20'>
+                {/* Job Details Header Section */}
+                <section>
+                    <div className='flex items-center justify-center gap-20 '>
+                        <div className='lg:relative lg:left-44'>
+                            <h1 className="text-4xl font-bold text-center ">Job Details</h1>
+                        </div>
+                        <div className='relative lg: left-44' >
+                            <Lottie className=' h-72 relative ' animationData={animation} loop={true} />;
 
-                </div>
-                <div>
-                    <h1 className="text-4xl font-bold text-center">Job Details</h1>
-                </div>
-
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    {/* Job description section */}
+                    <div >
+                        <div>
+                            <div className='mb-10'>
+                                <h1 className="text-xl font-bold">Job Description <br /> <span className='text-gray-600 font-normal'>{job.job_description}</span></h1>
+                            </div>
+                            <div className='mb-10'>
+                                <h1 className="text-xl font-bold">Job Responsibilities <br /> <span className='text-gray-600 font-normal'>{job.job_responsibilities}</span></h1>
+                            </div>
+                            <div className='mb-10'>
+                                <h1 className="text-xl font-bold">Educational Requirement: <span className='text-gray-600 font-normal'>{job.educational_requirements}</span></h1>
+                            </div>
+                            <div className='mb-10'>
+                                <h1 className="text-xl font-bold">Experience <br /> <span className='text-gray-600 font-normal'>{job.experiences}</span></h1>
+                            </div>
+                        </div>
+                        {/* job details side section */}
+                        <div>
+                            <div>
+                                <h1 className="text-xl font-bold mb-3">Job Details</h1>
+                                <hr />
+                                <div className='flex items-center gap-2 mb-5'>
+                                <CurrencyDollarIcon className='h-6'></CurrencyDollarIcon>
+                                <h1 className="text-xl font-semibold">   Salary: <span className='text-normal text-gray-600'>{job.salary} Per month</span></h1>
+                                </div>
+                                <div className='flex items-center gap-2 mb-5'>
+                                <CalendarDaysIcon className='h-6'></CalendarDaysIcon>
+                                <h1 className="text-xl font-semibold">  Job Title: <span className='text-normal text-gray-600'></span></h1>
+                                </div>
+                            </div>
+                            <div>
+                            <h1 className="text-xl font-bold mb-3">Contact Information</h1>
+                                <hr />
+                                <div className='flex items-center gap-2 mb-5'>
+                                <PhoneIcon className='h-6'></PhoneIcon>
+                                <h1 className="text-xl font-semibold">   Phone: <span className='text-normal text-gray-600'>{job.contact_information.phone} </span></h1>
+                                </div>
+                                <div className='flex items-center gap-2 mb-5'>
+                                <EnvelopeIcon className='h-6'></EnvelopeIcon>
+                                <h1 className="text-xl font-semibold">  Email: <span className='text-normal text-gray-600'></span></h1>
+                                </div>
+                                <div className='flex items-center gap-2 mb-5'>
+                                <MapPinIcon className='h-6'></MapPinIcon>
+                                <h1 className="text-xl font-semibold">  Location: <span className='text-normal text-gray-600'>{job.location} </span></h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     );
